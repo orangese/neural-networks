fixing average_GL

digit_classifier/digit_classifier_code/digit_classifier_nn.py
@@ -167,9 +167,6 @@ class Early_Stop(object):
     exceeds a parameter. If a new accuracy maximum has been found, this function
     returns "new". Otherwise, the function returns None"""
     local_opt = max(accuracy)
-##    accuracy = accuracy[np.argmax(accuracy):]
-##    """the above line ensures that GL is only evaluated after the
-##    locally optimal point"""
     if len(accuracy) == 0:
       return "new"
     elif len(accuracy) >= n:
@@ -257,6 +254,7 @@ class Network(object):
     if lr_variation:
       original_lr = learning_rate
       change_lr = False
+      to_evaluate = []
       """format for lr_variation parameter is [GL_type, stop_parameter,
       aGL_strip_GL_parameter, lr_variation_parameter, lr_variation_cutoff]"""
     
@@ -296,8 +294,10 @@ class Network(object):
       else:
         validation_evaluate = self.evaluate_accuracy(validation_data)
         print ("Epoch {0}: {1}%".format(epoch_num + 1, validation_evaluate))
-        if early_stopping or monitor or lr_variation:
+        if early_stopping or monitor:
           evaluation["validation accuracy"].append(validation_evaluate)
+        if lr_variation:
+          to_evaluate.append(validation_evaluate)
         if monitor:
           evaluation["train accuracy"].append(self.evaluate_accuracy(
             training_data, is_train = True))
@@ -327,17 +327,16 @@ class Network(object):
 
       if lr_variation:
         if lr_variation[0] == "GL":
-          change_lr = Early_Stop.GL(evaluation["validation accuracy"],
-                                  lr_variation[1])
+          change_lr = Early_Stop.GL(to_evaluate, lr_variation[1])
         elif lr_variation[0] == "average_GL":
-          change_lr = Early_Stop.average_GL(evaluation["validation accuracy"],
-                                          lr_variation[1], lr_variation[2])
+          change_lr = Early_Stop.average_GL(to_evaluate, lr_variation[1],
+                                            lr_variation[2])
         elif lr_variation[0] == "strip_GL":
-          change_lr = Early_Stop.strip_GL(evaluation["validation accuracy"],
-                                        lr_variation[1], lr_variation[2])
-
+          change_lr = Early_Stop.strip_GL(to_evaluate, lr_variation[1],
+                                          lr_variation[2])
         if change_lr == "stop":
           learning_rate /= lr_variation[3]
+          to_evaluate = []
         if original_lr * lr_variation[4] >= learning_rate:
           print ("End SGD: learning rate parameter exceeded")
           break

digit_classifier/digit_classifier_code/mnist_loader.py
@@ -70,7 +70,7 @@ import numpy as np #parsing the files
 import gzip #for unzipping files
 
 def to_int(b):
-  #function that takes in a bytearray and returns an integer
+  #function that takes in a byte array and returns an integer
   return int(codecs.encode(b, "hex"), 16)
 
 def normalize(raw_array, range_):
@@ -116,8 +116,8 @@ def load_file(file, mode):
     if magic_number == 2049: #2049 is the magic number for labels
       parsed = np.frombuffer(data, dtype = np.uint8, offset = 8)
       """almost all of the work is done by the line above. In essence, the line
-      above is converting the file from bytearray to a reshaped numpy array
-      with dimesions (60000,).
+      above is converting the file from byte array to a re-shaped numpy array
+      with dimensions (60000,).
       (Note the difference between (60000,) and (60000, 1): an array of shape
       (60000,) is a 1-D array of length 60000, while an array of
       shape (60000, 1) is a 60000-D array with each dimension of length 1"""
@@ -130,7 +130,7 @@ def load_file(file, mode):
       """the next four items give the number of columns in one image"""
       parsed = normalize(np.frombuffer(data, dtype = np.uint8, offset = 16).
                          reshape(length, num_rows * num_columns, 1), (0, 255))
-      """converting the file from bytearray to reshaped numpy array with
+      """converting the file from byte array to re-shaped numpy array with
        dimensions (length, num_rows * num_columns, 1) in order to prepare it
        for usage in the digit_classifier program."""
     else: