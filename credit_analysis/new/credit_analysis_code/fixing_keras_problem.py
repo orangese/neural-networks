a7188e97a57f5961c3e63c7ebf4910ca66788cfa
ryan <ryan@Johns-Air.fios-router.home>
2019-05-28 18:34:54 -0400

    fixing solution to keras/posting problem again

--- credit_analysis/new/credit_analysis_code/fixing_keras_problem.py
+++ credit_analysis/new/credit_analysis_code/fixing_keras_problem.py
@@ -12,7 +12,11 @@ import numpy as np
 
 #copy this function into "models.py"
 def predict(parsed_model, vars_):
-  return feed_forward(parsed_model, np.array([float(var) for var in vars_]))
+  #returns a, b, c, d, e, f, or g based on network prediction
+  range_ = ["A", "B", "C", "D", "E", "F", "G"]
+  prediction = feed_forward(parsed_model,
+                            np.array([float(var) for var in vars_]))
+  return range_[np.argmax(prediction)]
 
 #also goes in "models.py"
 def parse_net(model):
@@ -23,7 +27,6 @@ def parse_net(model):
   biases = np.array([model.layers[i].get_weights()[1]
                      for i in range(len(model.layers))])
   biases = biases.reshape(biases.shape[0], biases.shape[1], 1)
-  print (biases.shape, weights.shape)
   return (biases, weights)
 
 #also goes in "models.py"
@@ -42,18 +45,18 @@ def feed_forward(parsed_model, a):
 #step one: creating the sample model
 #("model" should be replaced with the credit analysis network in "models.py")
 model = keras.Sequential([
-  keras.layers.Dense(10, input_shape = (6, ), activation = "sigmoid")
+  keras.layers.Dense(7, input_shape = (5, ), activation = "sigmoid")
 ])
 model.compile(loss = "binary_crossentropy", optimizer = "adam",
               metrics = ["accuracy"])
 
 #step two: fake data
 #replace with real independent variables that will be fed into net
-fake_independent_vars = np.array([1, 2, 3, 4, 5, 6])
+fake_independent_vars = np.array([1, 2, 3, 4, 5])
 
 #step three: parse model in "models.py"
 parsed_model = parse_net(model)
 #don't forget, this line (and everything above it) goes in "models.py"!
 
-#step four: go to "view.py" and import feed_forward and import parsed_model
-print (feed_forward(parsed_model, fake_independent_vars))
+#step four: go to "view.py" and import predict and import parsed_model
+print (predict(parsed_model, fake_independent_vars))
