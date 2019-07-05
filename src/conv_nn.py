02994d2bcdc73d01f16d6b9099a6840a7dd99409
Ryan <ryan@Ethans-iMac.fios-router.home>
2019-07-05 09:18:17 -0400

    diagnosing problem with conv_nn.py

--- src/conv_nn.py
+++ src/conv_nn.py
@@ -94,15 +94,15 @@ from mlp import Activation, Cost
 
 #Classes
 class Layer(object):
-  """provides appropriate abstraction and acts as an input layer"""
+  #provides appropriate abstraction and acts as an input layer
 
   def __init__(self, dim):
     self.dim = dim
-    self.output = np.random.normal(loc = 0, size = self.dim)
+    self.output = np.random.normal(size = self.dim)
     #dim refers to the dimensions of the output of this layer
 
 class Conv(Layer):
-  """basic 2-D convolutional layer, stride is fixed at one"""
+  #basic 2-D convolutional layer, stride is fixed at one
 
   def __init__(self, kernel_dim, num_fmaps, actv = Activation("sigmoid"),
                previous_layer = None, next_layer = None):
@@ -118,7 +118,7 @@ class Conv(Layer):
       self.param_init()
 
   def param_init(self):
-    """initializes weights, biases, and gradients"""
+    #initializes weights, biases, and gradients
     self.biases = np.random.normal(size = (self.num_fmaps, 1, 1))
     n_out = self.num_fmaps * np.prod(self.kernel_dim)
     try: n_out /= np.prod(self.next_layer.pool_dim)
@@ -130,7 +130,7 @@ class Conv(Layer):
     self.nabla_w = np.zeros(self.weights.shape)
 
   def propagate(self, backprop = False):
-    """propagates through Conv layer, param_init not assumed"""
+    #propagates through Conv layer, param_init not assumed
     try: self.biases
     except AttributeError: self.param_init()
     zs = np.array([
@@ -142,35 +142,24 @@ class Conv(Layer):
     if backprop: self.zs = zs
 
   def backprop(self):
-    """backpropagation through Conv layer, forward pass assumed"""
+    #backpropagation through Conv layer, forward pass assumed
     if isinstance(self.next_layer, Pooling):
       self.error = self.next_layer.get_loc_fields(np.zeros(self.dim))
-      next_error = self.next_layer.error.reshape(*self.next_layer.dim, 1)
-      np.put_along_axis(self.error, self.next_layer.max_args, next_error,
+      np.put_along_axis(self.error, self.next_layer.max_args,
+                        self.next_layer.error.reshape(*self.next_layer.dim, 1),
                         axis = 3)
       self.error = self.next_layer.consolidate(self.error)
     else:
       self.error = np.dot(self.next_layer.weights.T, self.next_layer.error) * \
                    self.actv.derivative(self.zs).reshape(-1, 1)
-##      actv_deriv = self.actv.derivative(self.zs)
-##      for fmap_num in range(self.num_fmaps):
-##        self.error[fmap_num] = np.squeeze(
-##        np.dot(self.next_layer.weights[fmap_num].T, self.next_layer.error)) * \
-##        actv_deriv[fmap_num]
       
-##    self.nabla_b += np.sum(self.error, axis = (1, 2)).reshape(
-##      *self.biases.shape)
-##    self.nabla_w += np.array([
-##      convolve(self.previous_layer.output, np.rot90(self.error[fmap], 2),
-##               mode = "valid")
-##      for fmap in range(self.num_fmaps)])
     self.nabla_b += np.sum(self.error.reshape(self.num_fmaps, -1))
     self.nabla_w += np.array([
       convolve(self.previous_layer.output, err, mode = "valid")
       for err in self.error.reshape(*self.dim)])
 
   def param_update(self, lr, minibatch_size):
-    """weight and bias update"""
+    #weight and bias update
     self.biases -= (lr / minibatch_size) * self.nabla_b
     self.weights -= (lr / minibatch_size) * self.nabla_w
     
@@ -178,7 +167,7 @@ class Conv(Layer):
     self.nabla_w = np.zeros(self.weights.shape)
 
 class Pooling(Layer):
-  """basic pooling layer, for now, only 2-D max pooling is available"""
+  #basic pooling layer, for now, only 2-D max pooling is available
 
   def __init__(self, pool_dim, pool_type = "max", previous_layer = None,
                next_layer = None):
@@ -192,47 +181,44 @@ class Pooling(Layer):
     self.next_layer = next_layer
 
   def get_loc_fields(self, a):
-    """divides the convolutional output into local fields to prepare for \
-    pooling"""
+    #divides the convolutional output into local fields to prepare for pooling
     loc_fields = a.reshape(a.shape[0], int(a.shape[1] / self.pool_dim[0]),
                   self.pool_dim[0], int(a.shape[2] / self.pool_dim[1]),
                   self.pool_dim[1]).transpose(0, 1, 3, 2, 4)
     return loc_fields.reshape(*loc_fields.shape[:3], -1)
 
   def consolidate(self, a_):
-    """consolidates local fields into convolutional output"""
+    #consolidates local fields into convolutional output
     a = np.copy(a_).reshape(*a_.shape[:3], int(a_.shape[3] / 2),
                             int(a_.shape[3] / 2)).transpose(0, 1, 3, 2, 4)
     return a.reshape(a.shape[0], a.shape[1] * a.shape[2],
                      a.shape[3] * a.shape[4])
 
   def propagate(self, backprop = False):
-    """propagates through Pooling layer"""
+    #propagates through Pooling layer
     fmaps = self.get_loc_fields(self.previous_layer.output)
     if backprop:
+      try: print (self.cached == self.max_args)
+      except AttributeError: pass
       self.max_args = np.expand_dims(np.argmax(fmaps, axis = 3), axis = 3)
+      self.cached = self.max_args
     self.output = np.max(fmaps, axis = 3)
     self.dim = self.output.shape
 
   def backprop(self):
-    """backpropagation through Pooling layer, forward pass assumed"""
-##    self.error = np.empty(self.dim)
-##    for fmap_num in range(self.previous_layer.num_fmaps):
-##      self.error[fmap_num] = np.dot(self.next_layer.weights[fmap_num].T,
-##                                    self.next_layer.error.flatten())
+    #backpropagation through Pooling layer, forward pass assumed
     self.error = np.dot(self.next_layer.weights.T, self.next_layer.error)
     #activation of pooling layer is linear w.r.t. to the max activations in
     #the local pool, so the derivative of the activation function is 1
 
 class Dense(Layer):
-  """basic dense layer with multiple activation and cost functions"""
+  #basic dense layer with multiple activation and cost functions
 
-  def __init__(self, num_neurons, actv = Activation("sigmoid"), dropout_p = 1.0,
-               cost = None, previous_layer = None, next_layer = None):
+  def __init__(self, num_neurons, actv = Activation("sigmoid"), cost = None,
+               previous_layer = None, next_layer = None):
     self.num_neurons = num_neurons
     self.actv = actv
     self.cost = cost
-    self.dropout_p = dropout_p
     
     self.previous_layer = previous_layer
     self.next_layer = next_layer
@@ -243,45 +229,31 @@ class Dense(Layer):
     self.dim = (self.num_neurons, 1)
 
   def param_init(self):
-    """initializes layer parameters and gradients"""
-    self.biases = np.random.normal(size = (self.num_neurons, 1))    
-##    self.r_weights_shape = (self.num_neurons,
-##                            reduce(lambda a, b : a * b, self.previous_layer.dim))
-##    if isinstance(self.previous_layer, Conv) or \
-##       isinstance(self.previous_layer, Pooling):
-##      self.weights = np.random.normal(scale = np.sqrt(1.0 / self.num_neurons),
-##                                      size = (self.previous_layer.dim[0],
-##                                              self.num_neurons,
-##                                              *self.previous_layer.dim[1:]))
-##    else:
-##      self.weights = np.random.normal(scale = np.sqrt(1.0 / self.num_neurons),
-##                                     size = self.r_weights_shape)
-    #the reduce function flattens the previous layers's output so that
-    #computation is easier (especially with pooling layers)
+    #initializes layer parameters and gradients
+    self.biases = np.random.normal(size = (self.num_neurons, 1))
     self.weights = np.random.normal(scale = np.sqrt(1.0 / self.num_neurons),
                                     size = (self.num_neurons,
                                             reduce(lambda a, b : a * b,
                                                    self.previous_layer.dim)))
+    #the reduce function flattens the previous layers's output so that
+    #computation is easier (especially with pooling layers)
 
     self.nabla_b = np.zeros(self.biases.shape)
     self.nabla_w = np.zeros(self.weights.shape)
 
   def propagate(self, backprop = False):
-    """propagates through Dense layer, param init not assumed"""
+    #propagates through Dense layer, param init not assumed
     try: self.biases
     except AttributeError: self.param_init()
-##    reduced_weights = self.weights.reshape(self.r_weights_shape)
-##    zs = np.dot(reduced_weights, self.previous_layer.output.reshape(-1, 1)) \
-##         + self.biases
     zs = np.dot(self.weights, self.previous_layer.output.reshape(-1, 1)) \
                 + self.biases
     self.output = self.actv.calculate(zs)
     if backprop: self.zs = zs
 
   def backprop(self, img = None, label = None):
-    """backpropagation for Dense layer, forward pass assumed"""
+    #backpropagation for Dense layer, forward pass assumed
     if not (img is None):
-      self.error = self.cost.get_error(self.actv, self.output, self.zs, label) 
+      self.error = self.cost.get_error(self.actv, self.output, self.zs, label)
     else:
       self.error = np.dot(self.next_layer.weights.T, self.next_layer.error) * \
           self.actv.derivative(self.zs)
@@ -290,7 +262,7 @@ class Dense(Layer):
     self.nabla_w += np.outer(self.error, self.previous_layer.output)
 
   def param_update(self, lr, minibatch_size, reg = 0.0):
-    """weight and bias update, backprop assumed"""
+    #weight and bias update, backprop assumed
     self.nabla_w += (reg / minibatch_size) * self.weights
     #L2 regularization
 
@@ -317,7 +289,7 @@ class Network(object):
     if len(self.layers) != 0: self.layers[-1].cost = cost
 
   def add_layer(self, layer, position = None):
-    """adds a layer and links it to other layers"""
+    #adds a layer and links it to other layers
     new_layers = list(self.layers)
     if not position: position = len(self.layers)
     new_layers.insert(position, layer)
@@ -327,7 +299,7 @@ class Network(object):
     #to prevent manual addition of layers
 
   def rm_layer(self, position = None, layer = None):
-    """removes a layer and re-links it"""
+    #removes a layer and re-links it
     assert position or layer, "position or layer arguments must be provided"
     new_layers = list(self.layers)
     if layer: new_layers.remove(layer)
@@ -335,26 +307,27 @@ class Network(object):
     self.__init__(new_layers)
 
   def propagate(self, a, backprop = False):
-    """propagates input through network (similar to feed-forward)"""
+    #propagates input through network (similar to feed-forward)
     self.layers[0].output = a
     for layer in self.layers[1:]: layer.propagate(backprop = backprop)
     return self.layers[-1].output
 
   def backprop(self, img, label):
-    """backprop through network by piecing together Layer backprop functions"""
+    #backprop through network by piecing together Layer backprop functions
     self.propagate(img, backprop = True)
     self.layers[-1].backprop(img, label)
-    for layer in reversed(self.layers[1:len(self.layers) - 1]): layer.backprop()
+    for layer in reversed(self.layers[1:len(self.layers) - 1]):
+      layer.backprop()
 
   def param_update(self, lr, minibatch_size):
-    """network parameter update"""
+    #network parameter update
     for layer in self.layers[1:]:
       try: layer.param_update(lr, minibatch_size, reg = self.cost.reg_parameter)
       except TypeError: layer.param_update(lr, minibatch_size)
       except AttributeError: continue
 
   def SGD(self, train_data, num_epochs, lr, minibatch_size, val_data = None):
-    """stochastic gradient descent through network with L2 regularization"""
+    #stochastic gradient descent through network with L2 regularization
     for epoch_num in range(num_epochs):
       epoch = train_data
       np.random.shuffle(epoch)
@@ -369,32 +342,32 @@ class Network(object):
           epoch_num + 1, self.eval_acc(val_data), self.eval_cost(val_data)))
 
   def eval_acc(self, test_data):
-    """returns percent correct when the network is evaluated using test data"""
+    #returns percent correct when the network is evaluated using test data
     test_results = [(np.argmax(self.propagate(img)), label)
                     for (img, label) in test_data]
     return round(sum(int(img == label) for (img, label) in test_results) \
                   / len(test_data) * 100.0, 2)
 
   def eval_cost(self, test_data, is_train = False):
-    """returns cost when the network is evaluated using test data"""
+    #returns cost when the network is evaluated using test data
     return round(np.asscalar(self.cost.calculate([(self.propagate(
       img), self.vectorize(label)) for (img, label) in test_data])), 5)
 
   def vectorize(self, num):
-    """function that vectorizes a scalar (one-hot encoding)"""
+    #function that vectorizes a scalar (one-hot encoding)
     vector = np.zeros(self.layers[-1].dim)
     vector[num] = 1.0
     return vector
 
 #Testing area
 def generate_zero_data():
-  """generates zero data in the shape of MNIST data"""
+  #generates zero data in the shape of MNIST data
   data = {"train": [], "validation": [], "test": []}
   target = np.zeros((10, 1))
   target[0] = 1.0
-  data["train"] = [(np.ones((28, 28)), target) for i in range(10000)]
-  data["validation"] = [(np.ones((28, 28)), 0) for i in range(10000)]
-  data["test"] = [(np.ones((28, 28)), 0) for i in range(10000)]
+  data["train"] = [(np.ones((28, 28)), target) for i in range(1000)]
+  data["validation"] = [(np.ones((28, 28)), 0) for i in range(1000)]
+  data["test"] = [(np.ones((28, 28)), 0) for i in range(1000)]
   return data
 
 def test(net_type = "conv", data = None, test_acc = False, test_cost = False):
@@ -402,16 +375,19 @@ def test(net_type = "conv", data = None, test_acc = False, test_cost = False):
   
   if net_type == "conv":
     net = Network([Layer((28, 28)), Conv((5, 5), 20), Pooling((2, 2)),
-                   Dense(10, actv = Activation("sigmoid"))],
-                  cost = Cost("cross-entropy", reg_parameter = 0.0))
+##                   Dense(100),
+                   Dense(10, actv = Activation("softmax"))],
+                  cost = Cost("log-likelihood", reg_parameter = 0.0))
   elif net_type == "mlp":
     net = Network([Layer((28, 28)), Dense(100), Dense(10)])
 
   start = time()
 
-  print ("Evaluation without training: {0}%".format(net.eval_acc(data["test"])))
+  if test_acc:
+    print ("Evaluation without training: {0}%".format(
+      net.eval_acc(data["test"])))
   
-  net.SGD(data["train"][:1000], 5, 0.1, 10, data["validation"][:1000])
+  net.SGD(data["train"], 5, 0.1, 10, data["validation"])
 
   for i in range(10):
     pred = net.propagate(data["test"][i][0])
