71755e4242dc9451a74bff493b64691f23f910fe
ryan <ryan@Johns-Air.fios-router.home>
2019-07-06 20:43:09 -0400

    adding more customizable display functions

--- mnist/mnist-code/mnist_conv_display.py
+++ mnist/mnist-code/mnist_conv_display.py
@@ -10,18 +10,28 @@ A program to display the results of a trained ConvNN.
 #Libraries
 import sys
 sys.path.insert(0, "/Users/ryan/Documents/Coding/neural-networks/src")
-from conv_nn import Network, Layer, Conv, Pooling, Dense, test
+from conv_nn import Network, Layer, Conv, Pooling, Dense
 sys.path.insert(0, "/Users/ryan/Documents/Coding/neural-networks/\
                  mnist/mnist-code")
 from mnist_loader import load_data, display_image
 import matplotlib.pyplot as plt
 import numpy as np
+from time import time
 
-#Testing area
+#Graphics
+def closest_multiples(n):
+  #returns two multiples of n that are closest together
+  if n == 1 or n == 0: return (n, n)
+  factors = []
+  for i in range(1, n):
+    if n % i == 0: factors.append(((i, int(n / i)), (abs(i - int(n / i)))))
+  return factors[np.argmin(list(zip(*factors))[1])][0]
+
 def display(net, show_kernel = False, layer = Layer):
   #displays output of a layer or kernel weights using plt.imshow
   if show_kernel: layer = Conv
   disp_layer = next((l for l in net.layers if isinstance(l, layer)), None)
+  if disp_layer is None: return None
   disp_obj = np.copy(disp_layer.weights if show_kernel else disp_layer.output)
   fig, axes = plt.subplots() if layer is Layer \
               else plt.subplots(*closest_multiples(disp_layer.dim[0]))
@@ -37,23 +47,73 @@ def display(net, show_kernel = False, layer = Layer):
     axes.axis("off")
   plt.show()
 
-def closest_multiples(n):
-  #returns two multiples of n that are closest together
-  if n == 1: return (1, 1)
-  factors = []
-  for i in range(1, n):
-    if n % i == 0: factors.append(((i, int(n / i)), (abs(i - int(n / i)))))
-  return factors[np.argmin(list(zip(*factors))[1])][0]
+def display_net(net):
+  for layer in net.layers:
+    if isinstance(layer, Conv):
+      display(net, show_kernel = True)
+      display(net, layer = Conv)
+    elif isinstance(layer, Pooling): display(net, layer = Pooling)
+    elif isinstance(layer, Dense): continue
+    else: display(net, layer = Layer)
+  input("Press any key to continue: ")
+
+#Testing train functions
+def generate_zero_data():
+  #generates zero data in the shape of MNIST data
+  data = {"train": [], "validation": [], "test": []}
+  target = np.zeros((10, 1))
+  target[0] = 1.0
+  data["train"] = [(np.ones((28, 28)), target) for i in range(1000)]
+  data["validation"] = [(np.ones((28, 28)), 0) for i in range(1000)]
+  data["test"] = [(np.ones((28, 28)), 0) for i in range(1000)]
+  return data
+
+def test(net_type = "conv", data = None, shorten = False,
+         test_acc = False, test_cost = False):
+  #tests conv_nn.py
+  if data is None: data = generate_zero_data()
+  if shorten:
+    data["train"] = data["train"][:1000]
+    data["validation"] = data["validation"][:1000]
+    data["test"] = data["test"][:1000]
+  
+  if net_type == "conv":
+    net = Network([Layer((28, 28)),
+                   Conv((5, 5), 20, actv = "sigmoid"),
+                   Pooling((2, 2)),
+##                   Dense(100, actv = "sigmoid"),
+                   Dense(10, actv = "softmax", reg = 0.0)],
+                  cost = "log-likelihood")
+  elif net_type == "mlp":
+    net = Network([Layer((28, 28)), Dense(100, actv = "relu", reg = 5.0),
+                   Dense(10, actv = "softmax", reg = 5.0)],
+                  cost = "log-likelihood")
+
+  start = time()
+
+  if test_acc:
+    print ("Evaluation without training: {0}%".format(
+      net.eval_acc(data["test"])))
+  
+  net.SGD(data["train"], 50, 0.1, 10, data["validation"])
+
+  for i in range(10):
+    pred = net.propagate(data["test"][i][0])
+    if np.argmax(pred) != data["test"][i][1]:
+      print ("Ground truth: index {0} - {1}".format(
+        data["test"][i][1], round(np.asscalar(pred[data["test"][i][1]]), 5)))
+      print ("Max activation: index {0} - {1}".format(
+        np.argmax(pred), np.round(np.max(pred), 5)))
+      display_net(net)
+
+  if test_acc: print ("Accuracy: {0}%".format(net.eval_acc(data["test"])))
+  if test_cost: print ("Cost: {0}".format(net.eval_cost(data["test"])))
+  print ("Time elapsed: {0} seconds".format(round(time() - start, 3)))
+
+  return net
 
 if __name__ == "__main__":
-  np.seterr(all = "raise")
+##  np.seterr(all = "raise")
   data = load_data("conv")
-  data["train"] = data["train"][:1000]
-  data["validation"] = data["validation"][:1000]
-  data["test"] = data["test"][:1000]
-  net = test(net_type = input("MLP or ConvNN test? (mlp/conv): "), data = data)
-  display(net, show_kernel = True)
-  display(net, show_kernel = False, layer = Layer)
-  display(net, show_kernel = False, layer = Conv)
-  display(net, show_kernel = False, layer = Pooling)
-  input("Press any key to quit: ")
+  net = test(net_type = input("MLP or ConvNN test? (mlp/conv): "), data = data,
+             shorten = True)
