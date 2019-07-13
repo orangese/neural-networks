b3330fd28c0326f8b5cbeaee754a0ae912d4c41a
ryan <ryan@Ryans-MacBook-Air.local>
2019-07-13 15:20:51 -0400

    fixing merge issue over conv_nn.py

--- src/conv_nn.py
+++ src/conv_nn.py
@@ -43,8 +43,13 @@ class Conv(Layer):
   def param_init(self):
     #initializes weights, biases, and gradients
     self.biases = np.random.normal(size = (self.num_filters, 1, 1))
+<<<<<<< HEAD
     n_out = self.num_filters * np.prod(self.kernel_dim) / \
             np.prod(self.next_layer.pool_dim)
+=======
+    n_out = self.num_filters * np.prod(self.kernel_dim) \
+            / np.prod(self.next_layer.pool_dim)
+>>>>>>> old-state
     self.weights = np.random.normal(loc = 0, scale = np.sqrt(1.0 / n_out),
                                     size = (self.num_filters, *self.kernel_dim))
 
@@ -149,6 +154,10 @@ class Dense(Layer):
       self.biases = np.zeros(self.biases.shape)
       self.weights = np.zeros(self.weights.shape)
 
+    if self.actv.name == "softmax":
+      self.biases = np.zeros(self.biases.shape)
+      self.weights = np.zeros(self.weights.shape)
+
     self.nabla_b = np.zeros(self.biases.shape)
     self.nabla_w = np.zeros(self.weights.shape)
 
@@ -170,7 +179,15 @@ class Dense(Layer):
       self.error = self.cost.get_error(self.actv, self.output, self.zs, label)
 
     self.nabla_b += self.error
-    self.nabla_w += np.outer(self.error, self.previous_layer.output)
+    try:
+      self.nabla_w += np.outer(self.error, self.previous_layer.output)
+    except FloatingPointError:
+      print (np.linalg.norm(self.error))
+      print (np.linalg.norm(self.previous_layer.output))
+      print (self.error)
+      print (self.previous_layer.output)
+      print (self.next_layer)
+      raise FloatingPointError("dense backprop, nabla_")
 
   def param_update(self, lr, minibatch_size, epoch_len):
     #weight and bias update, backprop assumed
