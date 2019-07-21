trying to fix conv net backprop

mnist/mnist_code/mnist_conv_display.py
@@ -14,8 +14,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 from time import time
 
-np.random.seed(12345)
-
 #Graphics
 def closest_multiples(n):
   #returns two multiples of n that are closest together
@@ -113,6 +111,6 @@ def test(net = None, net_type = "conv", data = None, shorten = False, test_acc =
 if __name__ == "__main__":
   np.seterr(all = "raise")
   data = load_data("conv")
-  # net = test(net_type = input("MLP or ConvNN test? (mlp/conv): "), data = data, shorten = True, test_acc = True)
-  for i in range(10):
-    net = test(net_type = "conv", data = data, shorten = False, test_acc = True)
+  net = test(net_type = input("MLP or ConvNN test? (mlp/conv): "), data = data, shorten = True, test_acc = True)
+  # for i in range(10):
+  #   net = test(net_type = "conv", data = data, shorten = False, test_acc = True)

src/conv_nn.py
@@ -61,8 +61,8 @@ class Conv(Layer):
   def backprop(self):
     """backpropagation through Conv layer, forward pass assumed"""
     self.error = self.next_layer.get_loc_fields(np.zeros(self.dim))
-    np.put_along_axis(self.error, self.next_layer.max_args, self.next_layer.error.reshape(*self.next_layer.dim, 1),
-                      axis = -1)
+    np.put_along_axis(self.error, np.expand_dims(self.next_layer.max_args, axis = -1),
+                      self.next_layer.error.reshape(*self.next_layer.dim, 1), axis = -1)
     self.error = Pooling.consolidate(self.error)
 
     self.nabla_b += np.sum(self.error, axis = (1, 2))[..., np.newaxis, np.newaxis]
@@ -106,7 +106,7 @@ class Pooling(Layer):
   def propagate(self, backprop = False):
     """propagates through Pooling layer"""
     fmaps = self.get_loc_fields(self.previous_layer.output)
-    if backprop: self.max_args = np.expand_dims(np.argmax(fmaps, axis = -1), axis = -1)
+    if backprop: self.max_args = np.argmax(fmaps, axis = -1)
     self.output = np.max(fmaps, axis = -1)
     self.dim = self.output.shape
 