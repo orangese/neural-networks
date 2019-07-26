debugging conv_nn.py by fixing Conv backprop

mnist/mnist_code/mnist_conv_display.py
@@ -7,29 +7,32 @@ A program to display the results of a trained ConvNN.
 
 """
 
-#Libraries
+# Libraries
 from src.conv_nn import Network, Layer, Conv, Pooling, Dense
 from mnist.mnist_code.mnist_loader import load_data
 import matplotlib.pyplot as plt
 import numpy as np
 from time import time
 
-#Graphics
+import matplotlib
+matplotlib.use('TkAgg')
+
+# Graphics
 def closest_multiples(n):
-  #returns two multiples of n that are closest together
-  if n == 1 or n == 0: return n, n
+  # returns two multiples of n that are closest together
+  if n == 0 or n == 1: return n, n
   factors = [((i, int(n / i)), (abs(i - int(n / i)))) for i in range(1, n) if n % i == 0]
   return factors[np.argmin(list(zip(*factors))[1]).item()][0]
 
 def display(net, to_show = "output", layer = Layer):
-  #displays output of a layer or kernel weights using plt.imshow
+  # displays output of a layer or kernel weights using plt.imshow
   if to_show == "kernel": layer = Conv
   disp_layer = next((l for l in net.layers if isinstance(l, layer)), None)
   if disp_layer is None: return None
   if to_show == "output": disp_obj = np.copy(disp_layer.output)
   elif to_show == "kernel": disp_obj = np.copy(disp_layer.weights)
   elif to_show == "error": disp_obj = np.copy(disp_layer.error.reshape(disp_layer.dim))
-  
+
   fig, axes = plt.subplots() if layer is Layer else plt.subplots(*closest_multiples(disp_layer.dim[0]))
   fig.canvas.set_window_title("Visualizing convolutional networks")
   fig.suptitle("{0} for {1} layer".format(to_show, layer))
@@ -55,9 +58,9 @@ def display_net(net):
       display(net, layer = Layer)
   input("Press any key to continue: ")
 
-#Testing train functions
+# Testing train functions
 def generate_zero_data():
-  #generates zero data in the shape of MNIST data
+  # generates zero data in the shape of MNIST data
   data = {"train": [], "validation": [], "test": []}
   target = np.zeros((10, 1))
   target[0] = 1.0
@@ -67,7 +70,7 @@ def generate_zero_data():
   return data
 
 def test(net = None, net_type = "conv", data = None, shorten = False, test_acc = False):
-  #tests conv_nn.py
+  # tests conv_nn.py
   if data is None: data = generate_zero_data()
   if shorten:
     data["train"] = data["train"][:1000]
@@ -75,13 +78,13 @@ def test(net = None, net_type = "conv", data = None, shorten = False, test_acc =
     data["test"] = data["test"][:1000]
   
   if net_type == "conv":
-    net = Network([Layer((28, 28)), Conv((5, 5), 20, actv = "sigmoid"),
+    net = Network([Layer((28, 28)), Conv((5, 5), 3, actv = "sigmoid"),
                    Pooling((2, 2)), Dense(100, actv = "sigmoid", reg = 0.0),
                    Dense(10, actv = "softmax", reg = 0.0)],
                   cost = "log-likelihood")
   elif net_type == "mlp":
     net = Network([Layer((28, 28)),
-                   Dense(100, actv = "relu", reg = 0.0),
+                   Dense(100, actv = "sigmoid", reg = 0.0),
                    Dense(10, actv = "softmax", reg = 0.0)],
                   cost = "log-likelihood")
   elif net:
@@ -93,24 +96,25 @@ def test(net = None, net_type = "conv", data = None, shorten = False, test_acc =
 
   if test_acc: print ("Evaluation without training: {0}%".format(net.eval_acc(data["test"])))
   
-  net.SGD(data["train"], 60, 0.1, 10, data["validation"])
+  net.SGD(data["train"], 60, 0.5, 10, data["validation"])
 
-  for i in range(10):
-    pred = net.propagate(data["test"][i][0])
-    if np.argmax(pred) != data["test"][i][1]:
-      print ("Ground truth: index {0} - {1}".format(data["test"][i][1], round((pred[data["test"][i][1]]).item(), 5)))
-      print ("Max activation: index {0} - {1}".format(np.argmax(pred), round(np.max(pred), 5)))
-      display_net(net)
+  if test_acc:
+    for i in range(10):
+      pred = net.propagate(data["test"][i][0])
+      if np.argmax(pred) != data["test"][i][1]:
+        print ("Ground truth: index {0} - {1}".format(data["test"][i][1], round((pred[data["test"][i][1]]).item(), 5)))
+        print ("Max activation: index {0} - {1}".format(np.argmax(pred), round(np.max(pred), 5)))
+        display_net(net)
+    print ("Accuracy: {0}%".format(net.eval_acc(data["test"])))
 
-  if test_acc: print ("Accuracy: {0}%".format(net.eval_acc(data["test"])))
   print ("Time elapsed: {0} seconds".format(round(time() - start, 3)))
 
   return net
 
-#Testing area
+# Testing area
 if __name__ == "__main__":
   np.seterr(all = "raise")
   data = load_data("conv")
-  net = test(net_type = input("MLP or ConvNN test? (mlp/conv): "), data = data, shorten = True, test_acc = True)
-  # for i in range(10):
-  #   net = test(net_type = "conv", data = data, shorten = False, test_acc = True)
+  net = test(net_type = input("MLP or ConvNN test? (mlp/conv): "), data = data, shorten = False, test_acc = False)
+  #  for i in range(10):
+  #    net = test(net_type = "conv", data = data, shorten = False, test_acc = True)

src/conv_nn.py
@@ -9,23 +9,23 @@ are organized into their own classes.
 
 """
 
-#Imports
+# Imports
 import numpy as np
-from scipy.signal import convolve, convolve2d
+from scipy.signal import convolve2d
 from functools import reduce
 from src.mlp import Activation, Cost
 
-#Classes
+# Classes
 class Layer(object):
   """provides appropriate abstraction and acts as an input layer"""
 
   def __init__(self, dim):
     self.dim = dim
     self.output = np.zeros(self.dim)
-    #dim refers to the dimensions of the output of this layer
+    # dim refers to the dimensions of the output of this layer
 
 class Conv(Layer):
-  """convolutional layer with stride of 1 and no padding"""
+  """2-D convolutional layer with stride of 1 and no padding"""
 
   def __init__(self, kernel_dim, num_filters, actv = "sigmoid", previous_layer = None, next_layer = None):
     self.kernel_dim = kernel_dim
@@ -38,13 +38,10 @@ class Conv(Layer):
 
   def param_init(self):
     """initializes weights, biases, and gradients"""
-    self.num_fmaps = self.previous_layer.dim[0] if len(self.previous_layer.dim) == 3 else 1
-    
     self.biases = np.random.normal(size = (self.num_filters, 1, 1))
     scale = np.sqrt(1.0 / (self.num_filters * np.prod(self.kernel_dim) / np.prod(self.next_layer.pool_dim)))
-    size = [self.num_filters, *self.kernel_dim]
-    if self.num_fmaps != 1: size.append(self.num_fmaps)
-    self.weights = np.random.normal(loc = 0, scale = scale, size = size)
+    # weight init is improved by squashing standard deviation using 1 / sqrt(n_in)
+    self.weights = np.random.normal(scale = scale, size = (self.num_filters, *self.kernel_dim))
 
     self.nabla_b = np.zeros(self.biases.shape)
     self.nabla_w = np.zeros(self.weights.shape)
@@ -53,7 +50,7 @@ class Conv(Layer):
     """propagates through Conv layer, param_init not assumed"""
     try: self.biases
     except AttributeError: self.param_init()
-    zs = self.convolve(self.weights, self.previous_layer.output) + self.biases
+    zs = Conv.convolve(self.weights, self.previous_layer.output) + self.biases
     self.output = self.actv.calculate(zs)
     self.dim = self.output.shape
     if backprop: self.zs = zs
@@ -63,10 +60,11 @@ class Conv(Layer):
     self.error = self.next_layer.get_loc_fields(np.zeros(self.dim))
     np.put_along_axis(self.error, np.expand_dims(self.next_layer.max_args, axis = -1),
                       self.next_layer.error.reshape(*self.next_layer.dim, 1), axis = -1)
-    self.error = Pooling.consolidate(self.error)
+    # routes gradient such that only max activation neurons get nonzero gradient
+    self.error = Pooling.consolidate(self.error) * self.actv.derivative(self.zs)
 
-    self.nabla_b += np.sum(self.error, axis = (1, 2))[..., np.newaxis, np.newaxis]
-    self.nabla_w += self.convolve(self.previous_layer.output, self.error, is_err = True)
+    self.nabla_b += np.sum(self.error, axis = (1, 2)).reshape(self.nabla_b.shape)
+    self.nabla_w += Conv.convolve(self.previous_layer.output, self.error, is_err = True)
 
   def param_update(self, lr, minibatch_size):
     """weight and bias update"""
@@ -76,16 +74,13 @@ class Conv(Layer):
     self.nabla_b = np.zeros(self.biases.shape)
     self.nabla_w = np.zeros(self.weights.shape)
 
-  def convolve(self, a, b, is_err = False):
+  @staticmethod
+  def convolve(a, b, is_err = False):
     """convolves a_ with b_, order of convolution depends on err"""
-    if self.num_fmaps != 1:
-      if is_err: a, b = np.expand_dims(b, axis = -1), a
-      return np.squeeze([convolve(b.T, a_, "valid") for a_ in a])
-    else:
-      if is_err: return np.array([convolve2d(a, b_, "valid") for b_ in np.rot90(b, 2)])
-      else: return np.array([convolve2d(a_, b, "valid") for a_ in np.rot90(a, 2)])
-      #scipy.signal.convolve is consistent with the mathematical definition of convolve (which differs from the
-      #usual machine learning definition of convolve), so rot180 must be applied to accomodate for the discrepancy
+    if is_err: return np.array([convolve2d(a, np.rot90(b_, 2), mode = "valid") for b_ in b])
+    else: return np.array([convolve2d(np.rot90(a_, 2), b, mode = "valid") for a_ in a])
+    # scipy.signal.convolve is consistent with the mathematical definition of convolve (which differs from the
+    # usual machine learning definition of convolve), so rot180 must be applied to accomodate for the discrepancy
 
 class Pooling(Layer):
   """2-D max pooling layer"""
@@ -102,6 +97,7 @@ class Pooling(Layer):
     loc_fields = a.reshape(a.shape[0], int(a.shape[1] / self.pool_dim[0]), self.pool_dim[0],
                            int(a.shape[2] / self.pool_dim[1]), self.pool_dim[1]).transpose(0, 1, 3, 2, 4)
     return loc_fields.reshape(*loc_fields.shape[:3], -1)
+    # example: divides input with shape (20, 24, 24) into array with shape (20, 12, 12, 4)
 
   def propagate(self, backprop = False):
     """propagates through Pooling layer"""
@@ -113,14 +109,15 @@ class Pooling(Layer):
   def backprop(self):
     """backpropagation through Pooling layer, forward pass assumed"""
     self.error = np.dot(self.next_layer.weights.T, self.next_layer.error)
-    #activation of pooling layer is linear w.r.t. to the max activations in
-    #the local pool, so the derivative of the activation function is 1
+    # activation of pooling layer is linear w.r.t. to the max activations in
+    # the local pool, so the derivative of the activation function is 1
 
   @staticmethod
   def consolidate(a_):
     """consolidates local fields into convolutional output"""
     a = a_.reshape(*a_.shape[:3], int(a_.shape[3] / 2), int(a_.shape[3] / 2)).transpose(0, 1, 3, 2, 4)
     return a.reshape(a.shape[0], a.shape[1] * a.shape[2], a.shape[3] * a.shape[4])
+    # example: consolidates input with shape (20, 12, 12, 4) into array with shape (20, 24, 24)
 
 class Dense(Layer):
   """basic dense layer with multiple activation and cost functions"""
@@ -140,8 +137,9 @@ class Dense(Layer):
     self.biases = np.random.normal(size = (self.num_neurons, 1))
     self.weights = np.random.normal(scale = np.sqrt(1.0 / self.num_neurons),
                                     size = (self.num_neurons, reduce(lambda a, b : a * b, self.previous_layer.dim)))
-    
+
     if self.actv.name == "softmax":
+      # weight init for softmax follows the book "Neural Networks and Deep Learning"
       self.biases = np.zeros(self.biases.shape)
       self.weights = np.zeros(self.weights.shape)
 
@@ -170,6 +168,7 @@ class Dense(Layer):
     """weight and bias update, backprop assumed"""
     self.biases -= (lr / minibatch_size) * self.nabla_b
     self.weights = (1.0 - lr * self.reg / epoch_len) * self.weights - (lr / minibatch_size) * self.nabla_w
+    # update rule includes built-in L2 regularization
 
     self.nabla_b = np.zeros(self.biases.shape)
     self.nabla_w = np.zeros(self.weights.shape)
@@ -179,11 +178,11 @@ class Network(object):
 
   def __init__(self, layers = [], cost = "cross-entropy"):
     self.layers = tuple(layers)
-    #self.layers is a tuple to prevent manual addition of layers
+    # self.layers is a tuple to prevent manual addition of layers
     assert len(self.layers) == 0 or isinstance(self.layers[0], Layer), "network layers must start with Layer object"
     for previous_layer, layer in zip(self.layers, self.layers[1:]): layer.previous_layer = previous_layer
     for next_layer, layer in zip(self.layers[1:], self.layers): layer.next_layer = next_layer
-    #above loop link all the layers together
+    # above loops link all the layers together
     self.cost = Cost(cost)
     if len(self.layers) != 0: self.layers[-1].cost = self.cost
 
@@ -193,9 +192,8 @@ class Network(object):
     if not position: position = len(self.layers)
     new_layers.insert(position, layer)
     self.__init__(new_layers)
-    #it seems inefficient and un-pythonic to rewrite self.layers each time
-    #you want to add a new layer, but it seems to be the best way in order
-    #to prevent manual addition of layers
+    # it seems inefficient and un-pythonic to rewrite self.layers each time you want to add a new layer,
+    # but it seems to be the best way in order to prevent manual addition of layers
 
   def rm_layer(self, position = None, layer = None):
     """removes a layer and re-links it"""
@@ -245,8 +243,8 @@ class Network(object):
 
   def eval_cost(self, test_data):
     """returns cost when the network is evaluated using test data"""
-    return round((self.cost.calculate([(self.propagate(img), self.vectorize(label))
-                                       for (img, label) in test_data])).item(), 5)
+    return round(self.cost.calculate([(self.propagate(img), self.vectorize(label))
+                                      for (img, label) in test_data]).item(), 5)
 
   def vectorize(self, num):
     """function that vectorizes a scalar (one-hot encoding)"""

src/mlp.py
@@ -6,13 +6,13 @@ A program that houses a vanilla feed-forward neural network.
 
 """
 
-#Libraries
-import numpy as np #for fast matrix-based computations
-from time import time #for timing stuff
+#  Libraries
+import numpy as np # for fast matrix-based computations
+from time import time # for timing stuff
 
-#Classes
+# Classes
 class Cost(object):
-  #class for the different cost functions
+  # class for the different cost functions
 
   def __init__(self, name, regularization = None, reg_parameter = None):
     self.name = name
@@ -20,22 +20,21 @@ class Cost(object):
     self.reg_parameter = reg_parameter
 
   def derivative(self, a, y):
-    #returns ∂Cx/∂a_L
+    # returns ∂Cx/∂a_L
     if self.name == "mse": return a - y
     elif self.name == "cross-entropy": return (a - y) / (a * (1 - a))
     elif self.name == "log-likelihood": return y / a
 
   def calculate(self, pairs):
-    #accepts a list of tuples (a, y) and returns average cost over that list
+    # accepts a list of tuples (a, y) and returns average cost over that list
 
-    if self.name == "mse": cost = np.sum(np.linalg.norm(a - y) ** 2.0 for (a, y) in pairs) / (2.0 * len(pairs))
+    if self.name == "mse":
+      return np.sum(np.linalg.norm(a - y) ** 2.0 for (a, y) in pairs) / (2.0 * len(pairs))
     elif self.name == "cross-entropy":
-      cost = np.sum(np.sum(np.nan_to_num(-y * np.log(a) - (1.0 - y) * np.log(1.0 - a))
+      return np.sum(np.sum(np.nan_to_num(-y * np.log(a) - (1.0 - y) * np.log(1.0 - a))
                            for (a, y) in pairs)) / len(pairs)
     elif self.name == "log-likelihood":
-      cost = np.sum(np.nan_to_num(np.log(a[np.argmax(y)]) for (a, y) in pairs)) / (-1.0 * len(pairs))
-  
-    return cost
+      return np.sum(np.nan_to_num(np.log(a[np.argmax(y)]) for (a, y) in pairs)) / (-1.0 * len(pairs))
 
   def get_error(self, activation, activations, weighted_inputs, label):
     if self.name == "mse": return self.derivative(activations, label) * activation.derivative(weighted_inputs)
@@ -53,21 +52,24 @@ class Activation(object):
     elif self.name == "relu": return np.maximum(0, z)
 
   def derivative(self, z, j = None, i = None):
-    if self.name == "sigmoid": return self.calculate(z) * (1.0 - self.calculate(z))
+    if self.name == "sigmoid":
+      return self.calculate(z) * (1.0 - self.calculate(z))
     elif self.name == "softmax":
       assert i or j, "arguments 'i' or 'j' not provided"
       gradient = np.array([-1.0 * self.calculate(z_k)[j] * self.calculate(z_k)[i] if j != i else
                            self.calculate(z)[j] * (1.0 - self.calculate(z)[j]) for z_k in z])
       return gradient
-    elif self.name == "tanh": return 1.0 - np.power(self.calculate(z), 2)
-    elif self.name == "relu": return 1.0 * (z > 0.0)
+    elif self.name == "tanh":
+      return 1.0 - np.power(self.calculate(z), 2)
+    elif self.name == "relu":
+      return 1.0 * (z > 0.0)
 
 class Early_Stop(object):
-  #BEST METHOD: average improvement, others are just for demonstration
+  # BEST METHOD: average improvement, others are just for demonstration
   """Note that all the methods in this class need to be used in conjunction with some kind of loop-- they need to be
   supplemented with some other code"""
 
-  @staticmethod #method can be called without creating an Early_Stop object
+  @staticmethod # method can be called without creating an Early_Stop object
   def GL(accuracy, stop_parameter):
     """returns "stop" if the generalization loss exceeds a parameter. If a new accuracy maximum has been found, this
     function returns "new". Otherwise, the function returns None"""
@@ -120,23 +122,23 @@ class Network(object):
     self.output_activation = output_activation
 
   def regular_weight_init(self):
-    #squashes the distribution of pre-train weights, leading to better training
+    # squashes the distribution of pre-train weights, leading to better training
     self.weights = np.array([np.random.randn(next_layer, layer) / np.sqrt(layer) for layer, next_layer in zip(
       self.layers, self.layers[1:])])
     self.weight_init = "regular"
 
   def large_weight_init(self):
-    #does not squash distribution of pre-train weights
+    # does not squash distribution of pre-train weights
     self.weights = np.array([np.random.randn(next_layer, layer) for layer, next_layer in zip(
       self.layers, self.layers[1:])])
     self.weight_init = "large"
 
   def bias_init(self):
-    #regular bias initializer
+    # regular bias initializer
     self.biases = np.array([np.random.randn(layer, 1) for layer in self.layers[1:]])
     
   def feed_forward(self, a):
-    #feeds an input into the network and returns its output
+    # feeds an input into the network and returns its output
     for b, w in zip(self.biases[:-1], self.weights[:-1]): a = self.activation.calculate(np.dot(w, a) + b)
     a = self.output_activation.calculate(np.dot(self.weights[-1], a) + self.biases[-1])
     return a
@@ -153,7 +155,7 @@ class Network(object):
       stored_biases = self.biases
       stored_weights = self.weights
       to_stop = False
-      #format for early_stopping parameter is [GL_type, stop_parameter, aGL_strip_GL_parameter]
+      # format for early_stopping parameter is [GL_type, stop_parameter, aGL_strip_GL_parameter]
 
     if lr_variation:
       original_lr = learning_rate
@@ -223,7 +225,7 @@ class Network(object):
     if monitor or early_stopping: return evaluation
 
   def backprop(self, minibatch, dropout = None):
-    #calculates the gradients of the cost function w.r.t. weights and biases
+    # calculates the gradients of the cost function w.r.t. weights and biases
     a, labels = list(zip(*minibatch))
     a = np.array(a)
     labels = np.array(labels)
@@ -231,7 +233,7 @@ class Network(object):
     weighted_inputs = []
     activations = [a]
 
-    #Step 1: forward-propagating the data
+    # Step 1: forward-propagating the data
     for b, w in zip(self.biases, self.weights):
       z = np.dot(w, a).transpose(1, 0, 2) + b
       weighted_inputs.append(z)
@@ -242,27 +244,27 @@ class Network(object):
       activations[-1] = self.output_activation.calculate(weighted_inputs[-1])
 
     if dropout:
-      #implementation is not optimized-- loop is slow
+      # implementation is not optimized-- loop is slow
       dropout_matrix = self.dropout(dropout[0], dropout[1]) 
       activations = [act * drop for act, drop in zip(activations, dropout_matrix)]
 
     nabla_b = np.asarray([np.zeros(b.shape) for b in self.biases])
     nabla_w = np.asarray([np.zeros(w.shape) for w in self.weights])
     
-    #Step 2: computing the output error
+    # Step 2: computing the output error
     error = self.cost.get_error(self.output_activation, activations[-1], weighted_inputs[-1], labels)
     nabla_b[-1] = error
     nabla_w[-1] = np.array([np.outer(err, act) for err, act in zip(error, activations[-2])])
-    #loop is slow
+    # loop is slow
 
-    #Step 3: computing the errors for the rest of the layers
+    # Step 3: computing the errors for the rest of the layers
     l = len(self.layers) - 2
     while l > 0:
       error = np.dot(self.weights[l].T, error).transpose(1, 0, 2) * self.activation.derivative(weighted_inputs[l - 1])
-      #computing the error for layer "l + 1" (index "l")
+      # computing the error for layer "l + 1" (index "l")
       nabla_b[l - 1] = error
       nabla_w[l - 1] = np.array([np.outer(err, act) for err, act in zip(error, activations[l-1])])
-      #loop is slow
+      # loop is slow
       l -= 1
 
     nabla_b = np.array([np.sum(b, axis = 0) for b in nabla_b])
@@ -294,7 +296,7 @@ class Network(object):
     
     for epoch_num in range(num_epochs):
       epoch = training_data
-      np.random.shuffle(epoch) #randomly shuffle epoch
+      np.random.shuffle(epoch) # randomly shuffle epoch
       minibatches = [epoch[i:i + minibatch_size] for i in range(0, len(epoch), minibatch_size)]
 
       if momentum:
@@ -365,8 +367,8 @@ class Network(object):
     if monitor or early_stopping: return evaluation
 
   def unvectorized_backprop(self, image, label, dropout = None):
-    #calculates the gradients of the cost function w.r.t. weights and biases
-    #not vectorized but faster than vectorized if dropout is used
+    # calculates the gradients of the cost function w.r.t. weights and biases
+    # not vectorized but faster than vectorized if dropout is used
     weighted_inputs = []
     a = image
     activations = [a]
@@ -374,7 +376,7 @@ class Network(object):
     nabla_b = np.asarray([np.zeros(b.shape) for b in self.biases])
     nabla_w = np.asarray([np.zeros(w.shape) for w in self.weights])
 
-    #Step 1: forward-propagating the data
+    # Step 1: forward-propagating the data
     for b, w in zip(self.biases, self.weights):
       z = np.dot(w, a) + b
       weighted_inputs.append(z)
@@ -388,16 +390,16 @@ class Network(object):
       dropout_matrix = self.dropout(dropout[0], dropout[1]) 
       activations = np.array(activations) * dropout_matrix
     
-    #Step 2: computing the output error
+    # Step 2: computing the output error
     error = self.cost.get_error(self.output_activation, activations[-1], weighted_inputs[-1], label)
     nabla_b[-1] = error
     nabla_w[-1] = np.outer(error, activations[-2])
 
-    #Step 3: computing the errors for the rest of the layers
+    # Step 3: computing the errors for the rest of the layers
     l = len(self.layers) - 2
     while l > 0:
       error = np.dot(self.weights[l].T, error) * self.activation.derivative(weighted_inputs[l - 1])
-      #computing the error for layer "l + 1" (index "l")
+      # computing the error for layer "l + 1" (index "l")
       nabla_b[l - 1] = error
       nabla_w[l - 1] = np.outer(error, activations[l - 1])
       l -= 1
@@ -405,17 +407,17 @@ class Network(object):
     return nabla_b, nabla_w
 
   def dropout(self, dropout_layers, probabilities):
-    #returns a matrix of ones and zeros that is used to perform dropout
+    # returns a matrix of ones and zeros that is used to perform dropout
     assert len(self.layers) - 1 not in dropout_layers, "cannot apply dropout to output layer"
     
     dropout_matrix = np.array([np.ones((layer, 1)) for layer in self.layers])
     for layer, probability in zip(dropout_layers, probabilities):
       dropout_matrix[layer] = 1.0 * (np.random.random((self.layers[layer], 1)) <= probability)
-      #multiplying by one converts a boolean array to an integer array
+      # multiplying by one converts a boolean array to an integer array
     return dropout_matrix
 
   def evaluate_accuracy(self, test_data, is_train = False):
-    #returns percent correct when the network is evaluated using test data
+    # returns percent correct when the network is evaluated using test data
     test_results = [(np.argmax(self.feed_forward(image)), label) for (image, label) in test_data]
     if is_train:
       return round((sum(int(image == np.argmax(label)) for (image, label) in test_results) / len(test_data) * 100.0), 2)
@@ -423,22 +425,22 @@ class Network(object):
       return round((sum(int(image == label) for (image, label) in test_results) / len(test_data) * 100.0), 2)
 
   def evaluate_cost(self, test_data, is_train = False):
-    #returns cost when the network is evaluated using test data
+    # returns cost when the network is evaluated using test data
     if is_train:
       return self.cost.calculate([(self.feed_forward(image), label) for (image, label) in test_data])
     else:
       return self.cost.calculate([(self.feed_forward(image), self.vectorize(label)) for (image, label) in test_data])
 
   def vectorize(self, num):
-    #function that vectorizes a scalar (one-hot encoding)
+    # function that vectorizes a scalar (one-hot encoding)
     vector = np.zeros((self.layers[-1], 1))
     vector[num] = 1.0
     return vector
 
-  #Implementation function
+  # Implementation function
   def train(self, data, learning_rate, minibatch_size, num_epochs, momentum = None, dropout = None,
             early_stopping = None, lr_variation = None, monitor = False, show = True, write = None):
-    #implementation of SGD with backpropagation to calculate gradients
+    # implementation of SGD with backpropagation to calculate gradients
     start = time()
     
     if show: